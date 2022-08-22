af0d9fd7c444e54a372cbe181182f5849a396436
IronAdministrator <74884776+IronAdministrator@users.noreply.github.com>
2022-08-22 08:42:16 +0200

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from "@/components/Modal.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "My Vue App Title From Variable",
+      headerText: "Sign Up!",
+      headerTitle: "Modal Header",
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.inputText);
+      this.$refs.inputText.classList.add("active");
+    },
+  },
+};
 </script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="inputText" />
+  <button @click="handleClick">click</button>
+  <Modal :headerText="headerText" :headerTitle="headerTitle" theme="sale" />
+</template>
 
 <style>
 #app {
---
+++ src/components/Modal.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  props: ["headerText", "headerTitle", "theme"],
+};
+</script>
+
+<template>
+  <div class="backdrop">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <h1>{{ headerTitle }}</h1>
+      <p>{{ headerText }}</p>
+    </div>
+  </div>
+</template>
+
+<style>
+.backdrop {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+}
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.modal.sale {
+  background: black;
+  color: white;
+}
+</style>
